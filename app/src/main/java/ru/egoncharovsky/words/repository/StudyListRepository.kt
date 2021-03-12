6dc90679f785c242e574bd062af6638f41a8ed71
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2021-03-12 14:31:09 +0300

    corrected dimensions

--- app/src/main/java/ru/egoncharovsky/words/repository/StudyListRepository.kt
+++ app/src/main/java/ru/egoncharovsky/words/repository/StudyListRepository.kt
@@ -8,7 +8,7 @@ object StudyListRepository : InMemoryRepository<Long, StudyList>(LongIdGenerator
         listOf(
             StudyList(null, "Example List", DictionaryEntryRepository.getAll().take(20).toSet()),
             StudyList(null, "Example List 2", DictionaryEntryRepository.getAll().take(7).toSet()),
-            StudyList(null, "Example List 2", DictionaryEntryRepository.getAll().take(13).toSet())
+            StudyList(null, "Example List 3", DictionaryEntryRepository.getAll().take(13).toSet())
         ).forEach { save(it) }
     }
 
