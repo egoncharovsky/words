e78d5f83f98e21cfd4e1ee5dac558f11253b2519
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2022-01-11 22:31:50 +0300

    [18] hide keyboard on navigation

--- app/src/main/java/ru/egoncharovsky/words/ui/wordlist/ChooseWordsFragment.kt
+++ app/src/main/java/ru/egoncharovsky/words/ui/wordlist/ChooseWordsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StorageStrategy
@@ -16,6 +15,7 @@ import ru.egoncharovsky.words.databinding.FragmentChooseWordsItemBinding
 import ru.egoncharovsky.words.domain.entity.Word
 import ru.egoncharovsky.words.ui.SelectableRecyclerViewAdapter
 import ru.egoncharovsky.words.ui.dictionary.DictionaryFragment
+import ru.egoncharovsky.words.ui.navigateUp
 import ru.egoncharovsky.words.ui.setNavigationResult
 
 class ChooseWordsFragment : DictionaryFragment() {
@@ -52,7 +52,7 @@ class ChooseWordsFragment : DictionaryFragment() {
             chooseWordsViewModel.getChosenDictionaryIds().value?.let {
                 setNavigationResult(it.toLongArray())
             }
-            findNavController().navigateUp()
+            navigateUp()
         }
 
         tracker.onRestoreInstanceState(savedInstanceState)
